f6dadd711f5ae89a2b81155391e1f026f669502b
Ichim Cosmin-Stefan <73492909+ichim748@users.noreply.github.com>
2024-03-02 21:44:00 +0200

    Delete largeFileScript.py

--- largeFileScript.py
+++
@@ -1,8 +0,0 @@
-def create_large_txt_file(filename, size_in_mb):
-    num_chars = int(size_in_mb * 1e6)
-    with open(filename, 'w') as f:
-        f.write('0' * num_chars)
-
-
-if __name__ == '__main__':
-    create_large_txt_file('large.txt', 30)
