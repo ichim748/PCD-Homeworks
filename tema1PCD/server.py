4823b56546d02d17f7d46a55eb020ffedec8f420
Ichim Cosmin-Stefan <73492909+ichim748@users.noreply.github.com>
2024-03-18 21:03:45 +0200

    Update server.py
    Added functionality to write data received from socket to file.

--- tema1PCD/server.py
+++ tema1PCD/server.py
@@ -1,13 +1,13 @@
 import sys
 import socket
 
-
 connection_type = 'none'
 
-
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        exit(0)
+    if len(sys.argv) < 4:
+        print("Usage: <Protocol> <Buffer Size> <Transmission Mechanism>")
+        exit(1)
+
     host = socket.gethostname()
     port = 6787
 
@@ -15,38 +15,57 @@ if __name__ == '__main__':
         transmission_mechanism = 1
     elif sys.argv[3].lower() == 'stop-and-wait':
         transmission_mechanism = 0
-
-    if sys.argv[1] == 'TCP':
-        socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connection_type = 'TCP'
     else:
-        socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        connection_type = 'UDP'
+        print("Invalid transmission mechanism. Choose 'streaming' or 'stop-and-wait'.")
+        exit(1)
 
-    socket.bind((host, port))
+    if sys.argv[1].upper() == 'TCP':
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection_type = 'TCP'
+    elif sys.argv[1].upper() == 'UDP':
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        connection_type = 'UDP'
+    else:
+        print("Invalid protocol. Choose 'TCP' or 'UDP'.")
+        exit(1)
+
+    server_socket.bind((host, port))
     print(f'Server listening on {host}:{port} ({connection_type})')
 
     if connection_type == 'TCP':
-        socket.listen(1)
+        server_socket.listen(1)
+
     while True:
-        if connection_type == 'TCP':
-            connection, address = socket.accept()
+        connection = None
         number_of_messages_read = 0
         number_of_bytes_read = 0
-        while True:
-            if connection_type == 'TCP':
-                data = connection.recv(int(sys.argv[2]))
-                if transmission_mechanism == 0:
-                    connection.send('ACK'.encode('utf-8'))
-            else:
-                data, address = socket.recvfrom(int(sys.argv[2]))
-                if transmission_mechanism == 0:
-                    socket.sendto('ACK'.encode('utf-8'), address)
-            if data and data.decode('utf-8') != 'Done' and data.decode('utf-8') != 'ACK':
-                number_of_messages_read += 1
-                number_of_bytes_read += len(data)
-            else:
-                break
-        print(f"Server session: {connection_type}, messages read: {number_of_messages_read}, bytes read: {number_of_bytes_read}")
+
         if connection_type == 'TCP':
+            connection, address = server_socket.accept()
+
+        with open('received_data.txt', 'a') as file:
+            while True:
+                data = b''
+                if connection_type == 'TCP':
+                    data = connection.recv(int(sys.argv[2]))
+                    if transmission_mechanism == 0:
+                        connection.send('ACK'.encode('utf-8'))
+                else:
+                    data, address = server_socket.recvfrom(int(sys.argv[2]))
+                    if transmission_mechanism == 0:
+                        server_socket.sendto('ACK'.encode('utf-8'), address)
+
+                if data:
+                    message = data.decode('utf-8')
+                    if message != 'Done' and message != 'ACK':
+                        file.write(message + '\n')
+                        number_of_messages_read += 1
+                        number_of_bytes_read += len(data)
+                    else:
+                        break
+
+        print(f"Data written to file 'received_data.txt'")
+        print(f"Server session: {connection_type}, messages read: {number_of_messages_read}, bytes read: {number_of_bytes_read}")
+
+        if connection_type == 'TCP' and connection:
             connection.close()
